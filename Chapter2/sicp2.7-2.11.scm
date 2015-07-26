cc457ec919cc2614a298204afdbc9def44a2bd97
atsfour <timitikati@gmail.com>
2015-07-27 08:29:30 +0900

    solve 2.10

--- Chapter2/sicp2.7-2.11.scm
+++ Chapter2/sicp2.7-2.11.scm
@@ -12,6 +12,15 @@
   (make-interval (- (lower-bound x) (upper-bound y))
                  (- (upper-bound x) (lower-bound y))))
 
+; Exerxise 2.10
+(define (div-interval x y)
+  (if (and (< (lower-bound y) 0) (> (upper-bound y) 0))
+      (raise (condition (<error> (message "divisor spans 0"))))
+      (mul-interval
+        x
+        (make-interval (/ 1.0 (upper-bound y))
+                   (/ 1.0 (lower-bound y))))))
+
 (define (mul-interval x y)
   (let ((p1 (* (lower-bound x) (lower-bound y)))
         (p2 (* (lower-bound x) (upper-bound y)))
@@ -20,15 +29,8 @@
   (make-interval (min p1 p2 p3 p4)
                  (max p1 p2 p3 p4))))
 
-(define (div-interval x y)
-  (mul-interval
-    x
-    (make-interval (/ 1.0 (upper-bound y))
-                   (/ 1.0 (lower-bound y)))))
-
-
 
 ; Examples
 (define interval-1 (make-interval 1.0 3.0))
 (define interval-2 (make-interval 5.0 7.0))
-(define interval-3 (make-interval 2.0 2.0))
+(define interval-3 (make-interval -2.0 2.0))
