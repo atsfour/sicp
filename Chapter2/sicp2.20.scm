153f1ca3c87e9d1af4c6dc4ec6833b385f865329
atsfour <timitikati@gmail.com>
2015-07-30 01:32:36 +0900

    solve 2.20

--- Chapter2/sicp2.20.scm
+++ Chapter2/sicp2.20.scm
@@ -1,1 +1,15 @@
-;
+;Exercise 2.20
+(define (even? x)
+  (= (remainder x 2) 0))
+
+(define (my-filter f x)
+  (if (null? x)
+      `()
+      (if (f (car x))
+          (cons (car x) (my-filter f (cdr x)))
+          (my-filter f (cdr x)))))
+
+(define (same-parity n . x)
+  (if (even? n)
+      (cons n (my-filter even? x))
+      (cons n (my-filter (lambda (x) (not (even? x))) x))))
