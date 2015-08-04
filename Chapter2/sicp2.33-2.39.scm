2918e4c6712a90acd88ecbb8a0a455de38d367ab
atsfour <timitikati@gmail.com>
2015-08-04 23:57:43 +0900

    solve 2.36-2.37

--- Chapter2/sicp2.33-2.39.scm
+++ Chapter2/sicp2.33-2.39.scm
@@ -29,4 +29,30 @@
                                      (else (count-leaves x))))
                    t)))
 
+;Exercise 2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      ()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+;Exersise 2.37
+(define (dot-product v1 v2)
+  (accumulate + 0 (map * v1 v2)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product x v)) m))
+
+(define (transpose m)
+  (accumulate-n cons () m))
+
+(define (matrix-*-matrix m1 m2)
+  (let ((cols (transpose m2)))
+    (map (lambda (x) (matrix-*-vector cols x)) m1)))
+
 (define sample-list1 (list 1 2 3 4 5))
+(define vector1 (list 1 2 3))
+(define vector2 (list 4 5 6))
+(define matrix1 (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
+(define matrix2 (list (list 1 1 1) (list 1 1 1)))
+(define matrix3 (list (list 1 2) (list 3 4) (list 5 6)))
