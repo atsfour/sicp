205c6394cdceb472e776060927c881ebbf9b30d8
atsfour <timitikati@gmail.com>
2015-08-15 16:04:52 +0900

    solve 2.60

--- Chapter2/sicp2.60.scm
+++ Chapter2/sicp2.60.scm
@@ -16,3 +16,6 @@
 (define (union-set set1 set2)
   (append set2 set1))
 
+;adjoin-set と union-set から element-of-set?を取り除けるので、この２つは速くなる。
+;その反面、要素数が増えるのでelement-of-set自体は遅くなる。
+;ここでは必要ないがsetの内容を表示する時に、重複を除く手続きが必要になる。
